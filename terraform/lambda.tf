be347d2894781d48ca5542ea742d048f0493e493
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2024-11-22 21:56:28 -0300

    feat: hello world

---
+++ terraform/lambda.tf
@@ -0,0 +1,7 @@
+resource "aws_lambda_function" "my-lambda" {
+  function_name = "my_lambda"
+  role          = data.aws_iam_role.lab_role.arn
+  handler       = "main"
+  runtime       = "provided.al2"
+  filename      = "../bin/bootstrap.zip"
+}
