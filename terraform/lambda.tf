0d137e2619ef98a617d3aff5f248326c1a20eb4a
Jéssica Rodrigues <jessicarodridc@gmail.com>
2024-11-26 22:23:59 -0300

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,5 +3,5 @@ resource "aws_lambda_function" "my-lambda" {
   role          = data.aws_iam_role.lab_role.arn
   handler       = "main"
   runtime       = "provided.al2"
-  filename      = "../bin/bootstrap.zip"
+  filename      = "./bin/bootstrap.zip"
 }
