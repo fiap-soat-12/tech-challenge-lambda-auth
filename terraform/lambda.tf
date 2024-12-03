6243e062425cbace7c0a172e939d86b3b87e9221
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2024-12-03 20:54:56 -0300

    feat: change resources to terraform

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,7 +1,37 @@
-resource "aws_lambda_function" "my-lambda" {
-  function_name = "my_lambda"
+resource "aws_security_group" "lambda-sg" {
+  name        = "tech-challenge-lambda-authorizer-sg"
+  description = "FIAP Tech Challenge - Lambda Authorizer Security Group"
+  vpc_id      = data.aws_vpc.selected_vpc.id
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = data.aws_ip_ranges.api_gateway.cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lambda_function" "aws-lambda" {
+  function_name = "tech-challenge-lambda-authorizer"
+  description   = "FIAP Tech Challenge - Lambda Authorizer"
   role          = data.aws_iam_role.lab_role.arn
-  handler       = "main"
-  runtime       = "provided.al2"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.13"
   filename      = "../bin/bootstrap.zip"
+  vpc_config {
+    subnet_ids         = [for subnet in data.aws_subnet.selected_subnets : subnet.id]
+    security_group_ids = [aws_security_group.lambda-sg.id]
+  }
+
+  environment {
+    variables = {
+      AWS_REGION = var.aws_region
+    }
+  }
 }
